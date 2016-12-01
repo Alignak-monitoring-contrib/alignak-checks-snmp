b9fad05cd68eec9eaf87f1117624684f43265cf8
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-12-01 13:01:37 +0100

    Set version to 0.2.6

--- version.py
+++ version.py
@@ -18,7 +18,7 @@ __pkg_name__ = u"alignak_checks_snmp"
 __checks_type__ = u"snmp"
 
 # Application manifest
-__version__ = u"0.2.5"
+__version__ = u"0.2.6"
 __author__ = u"Frédéric MOHIER"
 __author_email__ = u"frederic.mohier@gmail.com"
 __copyright__ = u"(c) 2015 - %s" % __author__
