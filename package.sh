4b5ca54824247a73b616dcd13ae89530661b51d4
David Durieux <d.durieux@siprossii.com>
2017-09-30 13:44:56 +0200

    Fix version

--- package.sh
+++ package.sh
@@ -9,7 +9,7 @@ gem install fpm
 if [ "$1" = "master" ]; then
    if [ "$2" = "2.7" ]; then
      fpm -s python --python-install-lib "/usr/lib/python2.7/dist-packages" -t deb -a all -d python-alignak --deb-no-default-config-files --python-install-data '/usr/local' ./setup.py
-     version=`python -c "from alignak_backend_client import __version__;print(__version__)"`
+     version=`python -c "from version import __version__;print(__version__)"`
    fi
    sed -i -e "s|\"dev\"|\"${version}\"|g" .bintray.json
    sed -i -e s/alignak_deb-testing/alignak_deb-stable/g .bintray.json
